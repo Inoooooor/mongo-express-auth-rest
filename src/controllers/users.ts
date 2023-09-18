ae05c719ceab6b646ae8a51b11ab532b9d93c0b7
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-09-18 14:26:48 +0300

    added_user_removing_route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { getUsers } from "../db/users"
+import { getUsers, deleteUserById } from "../db/users"
 
 export const getAllUsers = async (
   req: express.Request,
@@ -13,5 +13,21 @@ export const getAllUsers = async (
   } catch (error) {
     console.log(error)
     return res.sendStatus(400)
+  }
+}
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params
+
+    const deletedUser = await deleteUserById(id)
+
+    return res.json(deletedUser)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
   }
 }
