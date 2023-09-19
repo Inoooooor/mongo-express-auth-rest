2cb1898e73c8d0f443cc5e8f0019f59a00143ffe
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-09-19 10:10:48 +0300

    isOwner&updateUser

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { getUsers, deleteUserById } from "../db/users"
+import { getUsers, deleteUserById, getUserById } from "../db/users"
 
 export const getAllUsers = async (
   req: express.Request,
@@ -26,6 +26,30 @@ export const deleteUser = async (
     const deletedUser = await deleteUserById(id)
 
     return res.json(deletedUser)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+  }
+}
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params
+    const { username } = req.body
+
+    if (!username) return res.sendStatus(400)
+
+    const user = await getUserById(id)
+
+    if (!user) return res.sendStatus(400)
+
+    user.username = username
+    await user.save()
+
+    return res.status(200).json(user).end()
   } catch (error) {
     console.log(error)
     res.sendStatus(400)
